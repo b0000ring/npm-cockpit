8a509e3bf11035183ae51d16f75053afe8243f16
Alex Chirkin <hello@alexchirkin.me>
2023-02-12 12:16:15 +0000

    minor fixes and improvements

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
+const { spawn } = require('child_process')
 
-const args = ['./__main__.py', ...process.argv.slice(2)]
-;
+console.log(__dirname)
 
-const pythonProcess = spawn('python', args);
+const args = [`${__dirname}/__main__.py`, ...process.argv.slice(2)]
 
-pythonProcess.stdout.setEncoding('utf8');
+const pythonProcess = spawn('python', args)
+
+pythonProcess.stdout.setEncoding('utf8')
 pythonProcess.stdout.on('data', (data) => {
-  console.log(data);
+  console.log(data)
 });
 
 pythonProcess.on('close', () => {
   pythonProcess.kill()
 });
 
-process.on('SIGINT', exit);
-process.on('exit', exit); 
-process.on('uncaughtException', exit);
+process.on('SIGINT', exit)
+process.on('exit', exit);
+process.on('uncaughtException', exit)
 
 function exit() {
   pythonProcess.kill()
